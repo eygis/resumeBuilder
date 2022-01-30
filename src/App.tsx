1d9a75383ff2bc906f2df251873d9b3622006012
eygis <86146074+eygis@users.noreply.github.com>
2022-01-30 10:06:54 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import './App.css'
+
+const App = () => {
+    
+    return (
+        <div id='wrapper'>
+         <h3>test</h3>
+        </div>
+    );
+};
+
+export default App;
