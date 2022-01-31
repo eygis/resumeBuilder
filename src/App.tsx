24733197a44e28c32904dd1c1d3ab984f3c2fea2
eygis <86146074+eygis@users.noreply.github.com>
2022-01-31 18:14:06 +0900

    22/1/31 welcome page

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,11 @@ const App = () => {
     
     return (
         <div id='wrapper'>
-         <h3>resumeBuilder</h3>
+         <div id='welcome'>
+          <p id='title'>Welcome!</p>
+          <p id='subtitle'>This quick app will guide you through and help you create a basic resume.</p>
+          <div id="startButton">Let's Get Started!<i className="fas fa-chevron-right"></i></div>
+         </div>
         </div>
     );
 };
