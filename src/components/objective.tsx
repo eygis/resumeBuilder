73ba171b9f9c543d5851754541d894ddcea59dbc
eygis <86146074+eygis@users.noreply.github.com>
2022-02-11 20:21:42 +0900

    22/2/11 text change

--- src/components/objective.tsx
+++ src/components/objective.tsx
@@ -4,7 +4,7 @@ let Objective = () : ReactElement => {
     return (
 <div id='objective'>
     <p className='pageType'>Objective</p>
-    <textarea id='objectiveTextArea' placeholder='Please write a brief summary of yourself and your career goals.' />
+    <textarea id='objectiveTextArea' placeholder='Please write a brief summary of yourself and your career objectives.' />
 </div>
     )}
 export default Objective
