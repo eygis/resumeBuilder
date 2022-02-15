d75efcf5c94360212ba55000bfe3aa892ee4630b
eygis <86146074+eygis@users.noreply.github.com>
2022-02-15 16:10:21 +0900

    wording change

--- src/components/history.tsx
+++ src/components/history.tsx
@@ -6,7 +6,7 @@ let History = () : ReactElement => {
     <p className='pageType'>Job History</p>    
     <input id="companyName" className='basicInput' type='text' placeholder='Company Name' required />
     <input id="jobTitle" className='basicInput' type='text' placeholder='Job Title' required />
-    <input id='years' className='basicInput' type='text' placeholder='Position Held Time Period' required />
+    <input id='years' className='basicInput' type='text' placeholder='Dates Between Which You Held the Position' required />
     <input id="jobDescription" className='basicInput' type='text' placeholder='Job Description' required />
 </div>
     )}
