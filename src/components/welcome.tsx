c798ff7aac1fe98db2080d99509f2aff3c4fe1ea
eygis <86146074+eygis@users.noreply.github.com>
2022-02-19 08:54:48 +0900

    22/2/19 wording change

--- src/components/welcome.tsx
+++ src/components/welcome.tsx
@@ -4,7 +4,7 @@ let Welcome = () : ReactElement => {
     return (
 <div id='welcome'>
     <p id='title'>Welcome!</p>
-    <p id='subtitle'>This will quickly and easily guide you through creating a basic resume/CV.</p>
+    <p id='subtitle'>This will quickly guide you through creating a basic resume/CV.</p>
 </div>
     )}
 export default Welcome
